d0a61815b7c9c28c38075a47e55d8c101ef8b5dd
Ryan Baseman <ryan.baseman@gmail.com>
2014-11-13 22:17:32 -0600

    Tweaking RCCar.js
    Still futzing around with adding some functions to control the RC Car.

--- code/RCCar.js
+++ code/RCCar.js
@@ -1,17 +1,50 @@
 var five = require("johnny-five");
-var myBoard, myLed;
+var myBoard, leftForward, leftBackward, rightForward, rightBackward;
 
 myBoard = new five.Board();
 
+// Setup pins and initialize RC wheels to be off
+// TODO - for some crazy reason on != on, so to turn things on you have to tur them off via code.
 myBoard.on("ready", function() {
 
-    myLed = new five.Led(13)
+    leftForward = new five.Led(13);
+    leftBackward = new five.Led(12);
 
-    // try "on", "off", "toggle", "brightness",
-    // "fade", "fadeIn", "fadeOut",
-    // "pulse", "strobe",
-    // "stop" (stops strobing, pulsing or fading)
+    rightForward = new five.Led(11);
+    rightBackward = new five.Led(10);
 
-    myLed.on();
+    stop();
 
+    setTimeout(forward(), 5000);
 });
+
+function forward() {
+
+    stop();
+
+    leftBackward.off();
+    rightBackward.off();
+}
+
+function reverse() {
+
+    stop();
+
+    leftForward.off();
+    rightForward.off();
+}
+
+function forwardLeft() {
+
+    stop();
+
+    leftForward.off();
+}
+
+function stop() {
+    leftForward.on();
+    leftBackward.on();
+
+    rightForward.on();
+    rightBackward.on();
+}
